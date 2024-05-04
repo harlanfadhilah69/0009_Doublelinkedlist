658cc1dec0171062b8c7463a00fc3f6802230b3d
harlanfadhilah69 <harlancoegg@gmail.com>
2024-05-04 10:04:19 +0700

    void deleteNode step 5

--- Double Linked List 2.cpp
+++ Double Linked List 2.cpp
@@ -108,5 +108,14 @@ void deleteNode()
 			START->prev = NULL;
 		}
 	}
-
+	else
+	{
+		previous->next = current->next;
+		if (current->next != NULL)
+		{
+			current->next->prev = previous;
+		}
+	}
+	delete current;
+	cout << "\x1b[32mRecord with roll number " << rollNo << " deleted\x1b[0m" << endl;
 }
