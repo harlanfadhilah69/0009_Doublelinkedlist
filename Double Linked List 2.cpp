7bc17babe963863501ce571e05bb22d1503ca600
harlanfadhilah69 <harlancoegg@gmail.com>
2024-05-04 10:18:43 +0700

    void search Data

--- Double Linked List 2.cpp
+++ Double Linked List 2.cpp
@@ -145,4 +145,37 @@ void revtraverse()
 {
 	if (listEmpty())
 		cout << "\nList is Empty" << endl;
+	else
+	{
+		cout << "\nRecords in descending order of roll number are:" << endl;
+		Node* currentNode = START;
+		while (currentNode->next != NULL)
+			currentNode = currentNode->next;
+		while (currentNode != NULL)
+		{
+			cout << currentNode->noMhs << " " << currentNode->name << endl;
+			currentNode = currentNode->prev;
+		}
+	}
+}
+
+void searchData()
+{
+	if (listEmpty() == true)
+	{
+		cout << "\nList is empty" << endl;
+	}
+	Node* prev, * curr;
+	prev = curr = NULL;
+	cout << "\nEnter the roll number of the student whose record you want to search: ";
+	int num;
+	cin >> num;
+	if (search(num, &prev, &curr) == false)
+		cout << "\nRecord not found" << endl;
+	else
+	{
+		cout << "\nRecord found" << endl;
+		cout << "\nRoll number: " << curr->noMhs << endl;
+		cout << "\nName: " << curr->name << endl;
+	}
 }
